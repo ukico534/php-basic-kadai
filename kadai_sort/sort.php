b21752d378a56d044ab8ab3ffa0bcdb632494c83
ukico534 <uki.kinoco@gmail.com>
2025-05-12 18:56:10 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -1,13 +1,13 @@
 <?php
 $nums = [15, 4, 18, 23, 10];
 
-function sort_and_display($array, $order) {
-    if ($order === 'asc') {
+function sort_2way($array, $is_asc) {
+    if ($is_asc === true) {
         echo "昇順にソートします。<br>";
-        sort($array); // 昇順
-    } elseif ($order === 'desc') {
+        sort($array);
+    } else {
         echo "降順にソートします。<br>";
-        rsort($array); // 降順
+        rsort($array);
     }
 
     foreach ($array as $num) {
@@ -15,7 +15,7 @@ function sort_and_display($array, $order) {
     }
 }
 
-sort_and_display($nums, 'asc');
-echo "<br>"; // 区切り
-sort_and_display($nums, 'desc');
+sort_2way($nums, true);   // 昇順
+echo "<br>";              // 区切り
+sort_2way($nums, false);  // 降順
 ?>
